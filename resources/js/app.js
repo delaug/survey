e0867cd89150bb931db1ab61eb5061424c17cba6
Sergei <delaug@bk.ru>
2021-10-25 22:29:21 +0300

    npm packages Vue and PrimeVue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,15 @@
 require('./bootstrap');
 
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// PrimeVue
+import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'                           //icons
+
+import PrimeVue from "primevue/config"
+
+createApp(App)
+    .use(PrimeVue)
+    .mount('#app')
