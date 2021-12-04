e8c9068b175b335fa0fbc9bcb9135e69cae4d8b0
Sergei <delaug@bk.ru>
2021-12-04 16:36:10 +0300

    php docs

--- app/Facades/User/AppFacade.php
+++ app/Facades/User/AppFacade.php
@@ -5,6 +5,11 @@ namespace App\Facades\User;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static int getNextQuestionPageID(int $survey_id)
+ * @method static \Illuminate\Contracts\Pagination\LengthAwarePaginator getSurveys()
+ * @method static mixed getSurvey(int $id)
+ * @method static \Illuminate\Contracts\Pagination\LengthAwarePaginator getQuestions(\App\Http\Requests\User\QuestionIndexRequest $request)
+ * @method static array setAnswer(\App\Http\Requests\User\AnswerStoreRequest $request)
  * @see \App\Services\User\AppService
  */
 class AppFacade extends Facade
